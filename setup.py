673740838b511bdee88684de9e9c9acf68d7d0fc
smestern <smestern@gmail.com>
2024-02-09 08:36:39 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sNMO',
+    version='0.1',
+    packages=["sNMO"],
+    install_requires=[
+        # List your package's dependencies here #TODO
+    ],
+    
+)
