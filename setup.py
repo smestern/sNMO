86b41a1746084783cd708074b38402acdd813015
smestern <smestern@gmail.com>
2024-02-09 08:46:16 -0500

    major reorg

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup, find_packages
 
+
+packages = find_packages()
+print(packages)
 setup(
     name='sNMO',
     version='0.1',
-    packages=["sNMO"],
+    packages=packages,
+    package_dir={'sNMO': '.'},
     install_requires=[
         # List your package's dependencies here #TODO
     ],
