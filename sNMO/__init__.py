29bd53d157518f3ce5f290ce1fd17a3ed87d7b90
smestern <smestern@gmail.com>
2024-02-09 09:06:15 -0500

    update setup

--- sNMO/__init__.py
+++ sNMO/__init__.py
@@ -1,2 +1,6 @@
-from . import *
+from . import error
+from . import optimizer
+from . import b2_model
+from . import utils
+
 #bring the optimizers to the top level
--- setup.py
+++ setup.py
@@ -1,13 +1,11 @@
 from setuptools import setup, find_packages
 
 
-packages = find_packages()
-print(packages)
+
 setup(
     name='sNMO',
     version='0.1',
-    packages=packages,
-    package_dir={'sNMO': '.'},
+    packages=['sNMO'],
     install_requires=[
         # List your package's dependencies here #TODO
     ],
